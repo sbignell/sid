49a7a69a21006207ff6c09b62a7e71dfba3a1e0a
steve <steve.bignell@gmail.com>
2015-02-16 20:29:13 +1100

    initial commit

--- config.example.js
+++ config.example.js
@@ -2,11 +2,11 @@
 
 exports.port = process.env.PORT || 3000;
 exports.mongodb = {
-  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/drywall'
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/revisit'
 };
-exports.companyName = 'Acme, Inc.';
-exports.projectName = 'Drywall';
-exports.systemEmail = 'your@email.addy';
+exports.companyName = 'progress labs';
+exports.projectName = 'revisit';
+exports.systemEmail = 'missioncontrol@revisit.cc';
 exports.cryptoKey = 'k3yb0ardc4t';
 exports.loginAttempts = {
   forIp: 50,
@@ -16,11 +16,11 @@ exports.loginAttempts = {
 exports.requireAccountVerification = false;
 exports.smtp = {
   from: {
-    name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
-    address: process.env.SMTP_FROM_ADDRESS || 'your@email.addy'
+    name: process.env.SMTP_FROM_NAME || exports.projectName,
+    address: process.env.SMTP_FROM_ADDRESS || 'missioncontrol@revisit.cc'
   },
   credentials: {
-    user: process.env.SMTP_USERNAME || 'your@email.addy',
+    user: process.env.SMTP_USERNAME || 'missioncontrol@revisit.cc',
     password: process.env.SMTP_PASSWORD || 'bl4rg!',
     host: process.env.SMTP_HOST || 'smtp.gmail.com',
     ssl: true
