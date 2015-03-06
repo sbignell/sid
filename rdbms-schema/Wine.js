eacf105302c94c1b706a035a22b87b10d5e72f0f
steve <steve.bignell@gmail.com>
2015-03-06 18:22:09 +1100

    updated wine schema

--- rdbms-schema/Wine.js
+++ rdbms-schema/Wine.js
@@ -2,16 +2,16 @@
 
 exports = module.exports = function(sequelize, DataTypes) {
   var Wine = sequelize.define('Wine', { 
-    id: { type: String, default: '' },
-    grape: { type: String, default: '' },
-    estate: { type: String, default: '' },
-    name: { type: String, default: '' },
-    notes: { type: String, default: '' },
-    rating: { type: String, default: '' },
-    createdAt: { type: Date, default: Date.now },
-    createdBy: { type: String, default: '' }
+    id: { type: Sequelize.INTEGER, autoIncrement: true },
+    grape: { type: Sequelize.STRING },
+    estate: { type: Sequelize.STRING },
+    name: { type: Sequelize.STRING },
+    notes: { type: Sequelize.STRING },
+    rating: { type: Sequelize.STRING },
+    createdBy: { type: Sequelize.STRING }
   });
  
+  //createdAt and updatedAt are auto-created by sequelize
   //return Wine;
   return Wine;
 
