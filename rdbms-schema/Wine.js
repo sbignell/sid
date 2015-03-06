e1e6f52bf5c5d83204f53a01e76b6c477b4ba1cf
steve <steve.bignell@gmail.com>
2015-03-06 18:23:51 +1100

    fixed typos

--- rdbms-schema/Wine.js
+++ rdbms-schema/Wine.js
@@ -2,13 +2,13 @@
 
 exports = module.exports = function(sequelize, DataTypes) {
   var Wine = sequelize.define('Wine', { 
-    id: { type: Sequelize.INTEGER, autoIncrement: true },
-    grape: { type: Sequelize.STRING },
-    estate: { type: Sequelize.STRING },
-    name: { type: Sequelize.STRING },
-    notes: { type: Sequelize.STRING },
-    rating: { type: Sequelize.STRING },
-    createdBy: { type: Sequelize.STRING }
+    id: { type: sequelize.INTEGER, autoIncrement: true },
+    grape: { type: sequelize.STRING },
+    estate: { type: sequelize.STRING },
+    name: { type: sequelize.STRING },
+    notes: { type: sequelize.STRING },
+    rating: { type: sequelize.STRING },
+    createdBy: { type: sequelize.STRING }
   });
  
   //createdAt and updatedAt are auto-created by sequelize
