9e647f4b34a518de78012b64dc9759397fa49177
steve <steve.bignell@gmail.com>
2015-03-06 18:29:26 +1100

    testing

--- rdbms-schema/Wine.js
+++ rdbms-schema/Wine.js
@@ -2,16 +2,15 @@
 
 exports = module.exports = function(sequelize, DataTypes) {
   var Wine = sequelize.define('Wine', { 
-    id: { type: sequelize.INTEGER, autoIncrement: true },
-    grape: { type: sequelize.STRING },
-    estate: { type: sequelize.STRING },
-    name: { type: sequelize.STRING },
-    notes: { type: sequelize.STRING },
-    rating: { type: sequelize.STRING },
-    createdBy: { type: sequelize.STRING }
+    id: { type: Number, default: '', autoIncrement: true },
+    grape: { type: String, default: '' },
+    estate: { type: String, default: '' },
+    name: { type: String, default: '' },
+    notes: { type: String, default: '' },
+    rating: { type: String, default: '' },
+    createdBy: { type: String, default: '' }
   });
  
-  //createdAt and updatedAt are auto-created by sequelize
   //return Wine;
   return Wine;
 
