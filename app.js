d8a1bd0bf5fb3ee2b9888de41af27752013f30a1
steve bignell <steve.bignell@gmail.com>
2015-12-19 22:33:58 +1100

    facebook test additional logging

--- app.js
+++ app.js
@@ -68,6 +68,12 @@ var sessionStore = new SessionStore({
     database: config.mysql.db
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 //settings
 app.disable('x-powered-by');
